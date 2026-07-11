693620f5b8eb683931570d6f7500fb0a7d3b82ed
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-07-11 21:35:16 +0900

    chore: configure git hooks using hk

---
+++ scripts/coverage.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")/.."
+
+if [ $# -eq 0 ]; then
+  target="$(go list ./... | fzf)"
+  profile=.coverage/$target/coverage.txt
+  mkdir -p .coverage/"$target"
+elif [ $# -eq 1 ]; then
+  target=$1
+  mkdir -p .coverage/"$target"
+  profile=.coverage/$target/coverage.txt
+  target=./$target
+else
+  echo "too many arguments are given: $*" >&2
+  exit 1
+fi
+
+go test "$target" -coverprofile="$profile" -covermode=atomic
+go tool cover -html="$profile"
